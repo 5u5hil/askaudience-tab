4e3ce746bbf1d95a1dbb70e529c54992e599d127
sachin619 <echo.smb@gmail.com>
2016-11-10 20:07:32 +0530

    d

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,7 @@
 // Ionic askaudience App
-var domain = 'http://askaudience.com/api/?method=';
+
+//var domain = 'http://askaudience.com/api/?method=';
+var domain = 'http://ask-audience.cruxservers.in/api/?method=';
 angular.module('askaudience', ['ionic', 'ngCordova', 'askaudience.controllers', 'askaudience.services', 'askaudience.directives', 'ion-datetime-picker', 'ngTagsInput', 'ion-autocomplete'])
 
         .run(function ($ionicPlatform, $cordovaStatusbar) {
