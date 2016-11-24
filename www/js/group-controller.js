f311cc8a07898c8aef1d1042d086ed04060f42f1
Sushil Sudhakaran <sushil@infiniteit.biz>
2016-11-24 12:51:19 +0530

    group UI

--- www/js/group-controller.js
+++ www/js/group-controller.js
@@ -24,6 +24,7 @@ app.controller('grpCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope', '$ion
                         text: 'Join Existing Group',
                         type: 'button-balanced',
                         onTap: function (e) {
+                            $scope.joinGroup();
                         }
                     },
                     {
@@ -44,9 +45,9 @@ app.controller('grpCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope', '$ion
                         text: 'Next',
                         type: 'button-positive',
                         onTap: function (e) {
-                            Loader.show();
+                            
                             if (jQuery('#my_group_name').val()) {
-
+                                Loader.show();
                                 var groupImg = jQuery('#group_image').prop('files')[0];
                                 var groupName = jQuery('#my_group_name').val();
                                 var groupForm = new FormData();
@@ -73,7 +74,43 @@ app.controller('grpCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope', '$ion
                                 ///////// redirect with response ID
                                 //$state.go('app.create-group', {id: 01});
                             } else {
-                                $scope.createGroup();
+                                $ionicPopup.alert({
+                                    template: 'Please Enter Group Name',
+                                    title: 'Group Name Required',                                   
+                                }).then(function(){
+                                     $scope.createGroup();
+                                });
+                               
+                            }
+                        }
+                    },
+                    {
+                        text: 'Cancel',
+                        type: 'button-default',
+                        onTap: function (e) {
+                        }
+                    }
+                ]
+            });
+        }
+        $scope.joinGroup = function () {
+            $scope.myPopup = $ionicPopup.alert({
+                scope: $scope,
+                template: '<form id="joinForm"  enctype="multipart/form-data"><ion-list><ion-item><input id="group_id" ng-model="group.groupId" type="text" name="group_id" placeholder="Enter Group Id" /></ion-item></ion-list></form>',
+                title: 'Join Group',
+                buttons: [
+                    {
+                        text: 'Join',
+                        type: 'button-balanced',
+                        onTap: function (e) {
+                            if (!jQuery('#group_id').val()) {
+                                $ionicPopup.alert({
+                                    template: 'Please try again',
+                                    title: 'Invalid Group Id',                                   
+                                }).then(function(){
+                                    $scope.joinGroup();
+                                });
+                                
                             }
                         }
                     },
@@ -93,12 +130,65 @@ app.controller('grpCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope', '$ion
 
             }
         ])
-        .controller('grpInfoCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope',
-            function ($scope, APIFactory, Loader, $rootScope) {
+        .controller('grpInfoCtrl', ['$scope', 'APIFactory', 'Loader', '$rootScope', '$ionicPopup',
+            function ($scope, APIFactory, Loader, $rootScope, $ionicPopup) {
                 $scope.activePan = 'members';
                 $scope.updatePan = function (tab) {
                     $scope.activePan = tab;
                 }
+
+                $scope.editGroup = function (id) {
+                    $scope.myPopup2 = $ionicPopup.alert({
+                        template: '<form id="createForm"  enctype="multipart/form-data"><ion-list><ion-item><input id="my_group_name" ng-model="group.groupName" type="text" name="group_name" placeholder="Enter Group Name" /></ion-item><ion-item><input type="file" id="group_image" ng-model="group.groupImg" name="group_image" placeholder="Enter Group Name" /></ion-item></ion-list></form>',
+                        scope: $scope,
+                        title: 'Edit Group',
+                        buttons: [{
+                                text: 'Next',
+                                type: 'button-positive',
+                                onTap: function (e) {
+                                    Loader.show();
+                                    if (jQuery('#my_group_name').val()) {
+
+                                        var groupImg = jQuery('#group_image').prop('files')[0];
+                                        var groupName = jQuery('#my_group_name').val();
+                                        var groupForm = new FormData();
+                                        groupForm.append('groupImg', groupImg);
+                                        groupForm.append('groupName', groupName);
+                                        groupForm.append('userId', LSFactory.get('user').ID);
+                                        APIFactory.createGroup(groupForm).then(function (response) {
+                                            if (response.data.errorType == 'success') {
+                                                Loader.hide();
+                                                $state.go('app.create-group', {id: 01});
+                                            } else {
+                                                Loader.toggleLoadingWithMessage(response.data.msg, 2000);
+                                            }
+
+                                        }, function (error) {
+                                            // $scope.found = [];
+                                        });
+
+
+
+
+
+                                        /////////// save group
+                                        ///////// redirect with response ID
+                                        //$state.go('app.create-group', {id: 01});
+                                    } else {
+                                        $scope.createGroup();
+                                    }
+                                }
+                            },
+                            {
+                                text: 'Cancel',
+                                type: 'button-default',
+                                onTap: function (e) {
+                                }
+                            }
+                        ]
+                    });
+                }
+
             }
         ])
 
